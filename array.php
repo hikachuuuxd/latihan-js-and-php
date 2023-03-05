da27d79a69330d029e26b17c1aa9ec16a97178de
nisrinasf <nisrinashofwahfirdaus@gmail.com>
2023-03-05 18:37:53 +0700

    tugas pak munder rev 1

--- array.php
+++ array.php
@@ -36,12 +36,13 @@ if(isset($_POST['angka'])){
 </head>
 <body>
     <form action="" method="post">
+        <label for="number">Masukkan Angka : </label><br>
         <input type="number" name = 'satu'>
         <input type="number" name = 'dua'>
         <input type="number" name = 'tiga'>
         <input type="number" name = 'empat'>
         <input type="number" name = 'lima'>
-        <button type="submit" name="angka">Angka</button>
+        <button type="submit" name="angka">Urutkan</button>
     </form>
 
     <h3>Angka Terkecil ke Terbesar :</h3>
@@ -49,7 +50,7 @@ if(isset($_POST['angka'])){
             <tr>
             
             <?php sort($angka); foreach($angka as $a  => $key):?>
-            <td><?php  echo $key; ?></td>
+            <td><?php  echo $key . ','; ?></td>
             <?php endforeach; ?>
             </tr>
         </table>
@@ -59,7 +60,7 @@ if(isset($_POST['angka'])){
         <table>
             <tr>
             <?php rsort($angka);  foreach($angka as $a => $key):?>
-            <td><?php echo $key; ?></td>
+            <td><?php echo $key . ','; ?></td>
             <?php endforeach; ?>
             </tr>
         </table>
@@ -72,7 +73,7 @@ if(isset($_POST['angka'])){
             foreach($angka as $a => $key):
             if($key % 2 == 1) :
             ?>
-            <td><?php echo $key; ?></td>
+            <td><?php echo $key . ','; ?></td>
             <?php endif; endforeach; ?>
             </tr>
         </table>
